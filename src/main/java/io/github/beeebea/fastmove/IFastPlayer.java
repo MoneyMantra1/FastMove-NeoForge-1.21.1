cd1d536b0ceb0816186a5abf2d6bfe07e0461708
MoneyMantra1 <minemantramc@gmail.com>
2026-03-17 23:20:16 -0400

    Delete src/main directory

--- src/main/java/io/github/beeebea/fastmove/IFastPlayer.java
+++
@@ -1,8 +0,0 @@
-package io.github.beeebea.fastmove;
-
-public interface IFastPlayer {
-    MoveState fastmove_getMoveState();
-    void fastmove_setMoveState(MoveState moveState);
-    void fastmove_setJumpInput(boolean input);
-
-}
