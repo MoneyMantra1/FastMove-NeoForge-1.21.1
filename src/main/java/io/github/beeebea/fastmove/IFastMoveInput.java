597f13861b5fe489a411b40e1843c932a13b2b24
MoneyMantra1 <minemantramc@gmail.com>
2026-03-17 23:12:38 -0400

    Delete src/main directory

--- src/main/java/io/github/beeebea/fastmove/IFastMoveInput.java
+++
@@ -1,8 +0,0 @@
-package io.github.beeebea.fastmove;
-
-public interface IFastMoveInput {
-    default boolean ismoveUpKeyPressed() { return false; }
-    default boolean ismoveDownKeyPressed() { return false; }
-    default boolean ismoveUpKeyPressedLastTick() { return false; }
-    default boolean ismoveDownKeyPressedLastTick() { return false; }
-}
